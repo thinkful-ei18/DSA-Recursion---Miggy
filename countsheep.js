806f7119c3f3a1baae454035eb694e6407c969d8
emptyelephants <emptyelephants@gmail.com>
2018-04-02 08:42:02 -0700

    sheep drills done

---
+++ countsheep.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+function countSheep(numSheep) {
+  if (numSheep<1){
+    return;
+  }
+  console.log(numSheep+' Another Sheep Jumped Over The Fence');
+  countSheep(numSheep-1);
+}
+
+console.log('running with 4');
+countSheep(4);
+console.log('running with 10');
+countSheep(10);
+console.log('running with -1');
+countSheep(-1);
